f0c60dde8e41369a2abad1a3a13b4f6da4c36158
unknown <kratikasingh397@gmail.com>
2024-04-03 23:18:37 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,6 @@
+<h2><b>ABOUT US</b></h2>
+<h4> The main aim of this Virtual Bulletin Board project is send notice to any
+department of the institution very fast and easily by saving the valuable time which
+is spend in paper work. 
+<p>
+    
